cc9d4f60341822d7e8d01957b4f51dca7c413cb5
unknown <mritunjayku99@gmail.com>
2024-01-28 22:09:45 +0530

    error handling file is added

---
+++ Promi/errorHandling.js
@@ -0,0 +1,12 @@
+// function fun(){
+//     console.log("fun is called");
+// }
+try{
+  fun();
+}
+catch(error){
+//    console.log(error.stack);
+// console.log(error.message);
+console.log(error.name);
+ 
+}
